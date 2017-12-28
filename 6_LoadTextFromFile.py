4587f57d890ea8b70dcad84878bd125f8d97a688
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-12-28 16:59:09 -0500

    Load data from file using csv as well as numpy. Adding two example data files

---
+++ 6_LoadTextFromFile.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt, csv, numpy as np
+
+fig = plt.figure()
+
+x = []
+y = []
+
+# Use csv import
+with open('example.txt', 'r') as csvfile:
+    # Read csv file
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+
+plt.plot(x, y, label='Loaded from file using csv!')
+
+# Use numpy loadtxt
+x, y = np.loadtxt('example2.txt', delimiter=',',
+                  unpack=True)  # unpack=False gives ValueError: too many values to unpack (expected 2)
+
+plt.plot(x, y, label='Loaded from file using numpy!', color='r')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()
+
+fig.savefig('loadtextfromfile.eps', format='eps', dpi=1200)
