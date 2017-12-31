61d3f9e75f7be3b0a4f2c1dcd489d44709a9dd5f
Dhrubojyoti Roy <royd@cse.osu.edu>
2017-12-30 20:00:38 -0500

    Reformatting

--- 7_LoadDataFromInternet.py
+++ 7_LoadDataFromInternet.py
@@ -10,7 +10,8 @@ stock_data = []
 split_source = source_code.split('\n')  # Split data into lines
 
 # Split each line into tokens (fields)
-for line in split_source[1:]: # [1:] ignores the header line "Date,Open,High,Low,Close,Adjusted_close,Volume"
+for line in split_source[1:]:
+        # [1:] ignores the header line "Date,Open,High,Low,Close,Adjusted_close,Volume" in list split_source
     split_line = line.split(',')
     if len(split_line) == 7:
         if 'values' not in line and 'labels' not in line:
@@ -20,19 +21,19 @@ for line in split_source[1:]: # [1:] ignores the header line "Date,Open,High,Low
 date, closep, highp, lowp, openp, adj_closep, volume = np.loadtxt(stock_data,
                                                                   delimiter=',',
                                                                   unpack=True,
-                                                      # %Y = full year. 2015
-                                                      # %y = partial year 15
-                                                      # %m = number month
-                                                      # %d = number day
-                                                      # %H = hours
-                                                      # %M = minutes
-                                                      # %S = seconds
-                                                      # E.g.: 12-06-2014 -> %m-%d-%Y
-                                                      converters={0: bytespdate2num('%Y-%m-%d')})
+                                                                  # %Y = full year. 2015
+                                                                  # %y = partial year 15
+                                                                  # %m = number month
+                                                                  # %d = number day
+                                                                  # %H = hours
+                                                                  # %M = minutes
+                                                                  # %S = seconds
+                                                                  # E.g.: 12-06-2014 -> %m-%d-%Y
+                                                                  converters={0: bytespdate2num('%Y-%m-%d')})
 # Using mdates.strpdate2num('%Y-%m-%d') directly on Python3 returns:
 # "TypeError: strptime() argument 0 must be str, not <class 'bytes'>"
 
-plt.plot_date(date, closep,'-', label='Price')
+plt.plot_date(date, closep, '-', label='Price')
 
 plt.xlabel('Date')
 plt.ylabel('Price')
